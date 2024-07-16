f4250e87a0608777e728fac2c4ba8a337c4fb308
gubsey <tylertoohey@proton.me>
2024-07-15 23:10:55 -0400

    zig not werkiung

--- 2022/d16/zig/main.zig
+++ 2022/d16/zig/main.zig
@@ -1,38 +1,60 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const map = try load(std.heap.page_allocator);
+    const map = try parse_valves(std.heap.page_allocator);
+    defer map.deinit();
 
-    var it = map.iterator();
+    var it = map.map.iterator();
     while (it.next()) |entry| {
         std.debug.print("{s}", .{entry});
     }
 }
 
-fn load(alloc: std.mem.Allocator) !std.StringHashMap(Valve) {
+fn parse_valves(alloc: std.mem.Allocator) !std.StringHashMap(Valve) {
     const buf = try std.fs.cwd()
         .readFileAlloc(alloc, "../example.txt", 99999999);
+    defer alloc.free(buf);
 
     var map = std.StringHashMap(Valve).init(alloc);
 
     var lines = std.mem.tokenizeAny(u8, buf, "\n");
     while (lines.next()) |line| {
         var it = std.mem.tokenizeAny(u8, line, " =;");
+
         var wordslist = std.ArrayList([]const u8).init(alloc);
+        defer wordslist.deinit();
+
         while (it.next()) |word| {
             try wordslist.append(word);
         }
-        var words = wordslist.items;
 
-        const name = words[1];
-        const rate = try std.fmt.parseInt(i32, words[5], 10);
-        var leads = std.ArrayList([]const u8).init(alloc);
-        try leads.appendSlice(words[10..]);
+        const rate = try std.fmt.parseInt(i32, alloc.dupe(u8, wordslist.items[5]), 10);
+        const leads = std.ArrayList([]const u8).fromOwnedSlice(alloc, wordslist.items[10..]);
 
-        try map.put(name, .{ .flow_rate = rate, .tunnels = leads });
+        try map.put(alloc.dupe(u8, wordslist.items[1]), .{ .flow_rate = rate, .tunnels = leads.toOwnedSlice() });
     }
 
     return map;
+}
+
+const MyMap = struct {
+    map: std.StringHashMap(Valve),
+    strings: [][]const u8,
+
+    fn deinit(self: *@This()) void {
+        self.map.deinit();
+        //self.strings.deinit();
+    }
+};
+
+fn hash_string(str: std.ArrayList(u8)) void {
+    std.hash.Fnv1a_32.hash(str.items);
+}
+
+test "parse values" {
+    var map = try parse_valves(std.testing.allocator);
+    std.debug.print("{s}", .{map.strings.items[0]});
+    map.deinit();
 }
 
 const Valve = struct {
