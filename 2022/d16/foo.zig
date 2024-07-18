7f40a631b4d736fb7cecb4db2a5709b292e690ee
gubsey <118205572+gubsey@users.noreply.github.com>
2024-07-18 20:06:40 +0000

    Pending changes exported from your codespace

--- 2022/d16/foo.zig
+++ 2022/d16/foo.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 const INF = "∞";
+const START = "AA";
 
 pub fn main() !void {
     var gba = std.heap.GeneralPurposeAllocator(.{}){};
@@ -20,6 +21,105 @@ pub fn main() !void {
 
     var valve_map = try ValveMap.from_lines(alc, lines);
     defer valve_map.deinit();
+
+    var fw = try FloydWarshall.from_valve_map(&valve_map);
+    defer fw.deinit();
+
+    while (try fw.next()) |_| {
+        //std.debug.print("\n", .{});
+        //fw.print();
+    }
+
+    var flows = std.StringHashMap(i32).init(alc);
+    defer flows.deinit();
+
+    var valve_iter = valve_map.map.iterator();
+    while (valve_iter.next()) |entry| {
+        const rate = entry.value_ptr.*.flow_rate;
+        if (rate == 0) continue;
+
+        const name = entry.key_ptr.*;
+
+        try flows.put(name, rate);
+    }
+
+    const path_map = try alc.create(std.StringHashMap(void));
+    path_map.* = std.StringHashMap(void).init(alc);
+
+    var pq = PQ.init(alc, {});
+    defer pq.deinit();
+    try pq.add(.{
+        .name = START,
+        .path = path_map,
+        .score = 0,
+        .time_remaining = 30,
+    });
+
+    var answers = std.ArrayList(i32).init(alc);
+
+    while (pq.removeOrNull()) |queue_item| {
+        defer alc.destroy(queue_item.path);
+        defer queue_item.path.deinit();
+
+        if (queue_item.path.count() == flows.count()) {
+            try answers.append(queue_item.score);
+        }
+
+        var flow_iter = flows.iterator();
+        while (flow_iter.next()) |flow| {
+            if (queue_item.path.contains(flow.key_ptr.*)) continue;
+            // delta time
+            const dt: i32 = fw.table.get(queue_item.name).?.get(flow.key_ptr.*).? + 1;
+            // remaining time
+            const rt = queue_item.time_remaining - dt;
+
+            if (rt < 0) continue;
+
+            const ds = rt * flow.value_ptr.*;
+            var new_path = try alc.create(std.StringHashMap(void));
+            new_path.* = try queue_item.path.clone();
+            try new_path.put(flow.key_ptr.*, {});
+
+            try pq.add(.{ .name = flow.key_ptr.*, .path = new_path, .score = queue_item.score + ds, .time_remaining = rt });
+        }
+    }
+
+    while (pq.removeOrNull()) |qi| {
+        defer qi.path.deinit();
+        defer alc.destroy(qi.path);
+    }
+
+    const answer_slice = try answers.toOwnedSlice();
+    defer alc.free(answer_slice);
+
+    std.debug.print("{d}", .{answer_slice});
+}
+
+const QueueItem = struct {
+    name: []const u8,
+    path: *std.StringHashMap(void),
+    score: i32,
+    time_remaining: i32,
+};
+
+fn queueFn(context: void, a: QueueItem, b: QueueItem) std.math.Order {
+    _ = context;
+    return std.math.order(a.score, b.score).invert();
+}
+
+const PQ = std.PriorityQueue(QueueItem, void, queueFn);
+
+fn part1(fw: FloydWarshall, flow_map: std.StringHashMap(i32)) u32 {
+    _ = fw;
+    _ = flow_map;
+}
+
+fn max(comptime T: type, a: T, b: T) T {
+    if (a < b) {
+        return b;
+    } else {
+        return a;
+    }
 }
 
 fn collect(comptime T: type, alc: std.mem.Allocator, iter: anytype) ![]T {
@@ -37,10 +137,12 @@ const Valve = struct {
 
 const ValveMap = struct {
     map: std.StringHashMap(Valve),
-    allocator: std.mem.Allocator,
+    alloc: std.mem.Allocator,
+    order: [][]const u8,
 
     fn from_lines(alc: std.mem.Allocator, lines: [][]const u8) !ValveMap {
         var map = std.StringHashMap(Valve).init(alc);
+        var order = std.ArrayList([]u8).init(alc);
         for (lines) |line| {
             var words_iter = std.mem.tokenizeAny(u8, line, " =;,");
             const words = try collect([]const u8, alc, &words_iter);
@@ -55,25 +157,111 @@ const ValveMap = struct {
             }
 
             try map.put(name, .{ .flow_rate = flow_rate, .tunnels = tunnels });
+            try order.append(name);
         }
-        return .{ .map = map, .allocator = alc };
+        return .{ .map = map, .alloc = alc, .order = try order.toOwnedSlice() };
     }
 
     fn deinit(self: *@This()) void {
         var valves = self.map.iterator();
         while (valves.next()) |entry| {
-            self.allocator.free(entry.key_ptr.*);
+            self.alloc.free(entry.key_ptr.*);
             var keys = entry.value_ptr.tunnels.keyIterator();
             while (keys.next()) |key| {
-                self.allocator.free(key.*);
+                self.alloc.free(key.*);
             }
             entry.value_ptr.tunnels.deinit();
         }
         self.map.deinit();
+        self.alloc.free(self.order);
+    }
+};
+
+const FloydWarshall = struct {
+    map: *ValveMap,
+    table: std.StringHashMap(*std.StringHashMap(i32)),
+    //table: [][]?i32,
+    alloc: std.mem.Allocator,
+    step: usize = 0,
+
+    fn from_valve_map(map: *ValveMap) !FloydWarshall {
+        var table = std.StringHashMap(*std.StringHashMap(i32)).init(map.alloc);
+        const keys = map.order;
+
+        for (0..keys.len, keys) |i, keyi| {
+            const valve = map.map.get(keyi).?;
+            var row = try map.alloc.create(std.StringHashMap(i32));
+            row.* = std.StringHashMap(i32).init(map.alloc);
+
+            for (0..keys.len, keys) |j, keyj| {
+                if (i == j) {
+                    try row.put(keyj, 0);
+                    continue;
+                }
+
+                if (valve.tunnels.get(keyj)) |_| {
+                    try row.put(keyj, 1);
+                }
+            }
+
+            try table.put(keyi, row);
+        }
+
+        return .{ .map = map, .table = table, .alloc = map.alloc };
     }
 
-    fn keysAlloc(self: *@This()) !void {
-        var keys = self.map.keyIterator();
-        return try collect([]const u8, self.allocator, &keys);
+    fn next(self: *@This()) !?void {
+        const keys = self.map.order;
+        if (self.step >= keys.len) return null;
+
+        const a = keys[self.step];
+
+        for (keys) |i| {
+            for (keys) |j| {
+                const ia = self.table.get(i).?.get(a) orelse continue;
+                const aj = self.table.get(a).?.get(j) orelse continue;
+                const sum = ia + aj;
+
+                if (self.table.get(i).?.get(j)) |n| {
+                    if (n <= sum) continue;
+                }
+
+                try self.table.get(i).?.put(j, sum);
+            }
+        }
+
+        self.step += 1;
+    }
+
+    fn deinit(self: *@This()) void {
+        var val_iter = self.table.valueIterator();
+        while (val_iter.next()) |row| {
+            row.*.deinit();
+            self.map.alloc.destroy(row.*);
+        }
+        self.table.deinit();
+    }
+
+    fn print(self: @This()) void {
+        const padding = 3;
+        std.debug.print("   ", .{});
+        for (self.map.order) |key| {
+            std.debug.print("{s:>[1]}", .{ key, padding });
+        }
+        std.debug.print("\n", .{});
+        for (self.map.order) |i| {
+            std.debug.print("{s} ", .{i});
+            const row = self.table.get(i).?;
+            for (self.map.order) |j| {
+                if (row.get(j)) |x| {
+                    var buf: [padding]u8 = undefined;
+                    const s = std.fmt.bufPrint(&buf, "{d}", .{x}) catch unreachable;
+                    std.debug.print("{s:[1]}", .{ s, padding });
+                } else {
+                    std.debug.print("{s:>[1]}", .{ INF, padding });
+                }
+            }
+            std.debug.print("\n", .{});
+        }
     }
 };
--- 2022/d16/stolen.py
+++ 2022/d16/stolen.py
@@ -1,5 +1,6 @@
-import sys, re
-lines = [re.split('[\\s=;,]+', x) for x in sys.stdin.read().splitlines()]
+import re
+
+lines = [re.split('[\\s=;,]+', x) for x in open("example.txt").read().splitlines()]
 G = {x[1]: set(x[10:]) for x in lines}
 F = {x[1]: int(x[5]) for x in lines if int(x[5]) != 0}
 I = {x: 1<<i for i, x in enumerate(F)}
@@ -13,12 +14,13 @@ def visit(v, budget, state, flow, answer):
     answer[state] = max(answer.get(state, 0), flow)
     for u in F:
         newbudget = budget - T[v][u] - 1
-        if I[u] & state or newbudget <= 0: continue
+        if I[u] & state or newbudget <= 0:
+            continue
         visit(u, newbudget, state | I[u], flow + newbudget * F[u], answer)
-    return answer    
+    return answer
 
 total1 = max(visit('AA', 30, 0, 0, {}).values())
 visited2 = visit('AA', 26, 0, 0, {})
-total2 = max(v1+v2 for k1, v1 in visited2.items() 
+total2 = max(v1+v2 for k1, v1 in visited2.items()
                    for k2, v2 in visited2.items() if not k1 & k2)
 print(total1, total2)
