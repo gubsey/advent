2525e45760fbd015404fc182f16e0c93d9616007
Uncle Gubsey <gubsey@pop-os.mynetworksettings.com>
2024-09-07 12:15:18 -0400

    h

--- 2022/d17/src/main.rs
+++ 2022/d17/src/main.rs
@@ -7,27 +7,7 @@ use std::{
 type Rock = Vec<Vec<bool>>;
 
 fn main() {
-    let stage = Stage {
-        board: vec![],
-        rocks: parse_rocks(include_str!("../rocks.txt"))
-            .into_iter()
-            .cycle()
-            .enumerate(),
-        moves: stdin()
-            .lines()
-            .next()
-            .unwrap()
-            .unwrap()
-            .chars()
-            .map(|c| match c {
-                '<' => Move::Left,
-                '>' => Move::Right,
-                _ => panic!("{c} is not a valid input"),
-            })
-            .collect::<Vec<_>>()
-            .into_iter()
-            .cycle(),
-    };
+    let stage = Stage::default();
 }
 
 fn parse_rocks(rock_strings: &str) -> Vec<Rock> {
@@ -50,6 +30,36 @@ struct Stage {
     board: Vec<[bool; 7]>,
     rocks: Enumerate<Cycle<IntoIter<Rock>>>,
     moves: Cycle<IntoIter<Move>>,
+}
+
+impl Stage {
+    fn mv(&mut self, mv: Move) {}
+}
+
+impl Default for Stage {
+    fn default() -> Self {
+        Stage {
+            board: vec![],
+            rocks: parse_rocks(include_str!("../rocks.txt"))
+                .into_iter()
+                .cycle()
+                .enumerate(),
+            moves: stdin()
+                .lines()
+                .next()
+                .unwrap()
+                .unwrap()
+                .chars()
+                .map(|c| match c {
+                    '<' => Move::Left,
+                    '>' => Move::Right,
+                    _ => panic!("{c} is not a valid input"),
+                })
+                .collect::<Vec<_>>()
+                .into_iter()
+                .cycle(),
+        }
+    }
 }
 
 #[derive(Clone, Copy)]
