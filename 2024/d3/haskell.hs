9465de375d79a9edea94c2153d4decfd868d99c7
gubsey <tylertoohey@proton.me>
2024-12-03 20:38:45 +0000

    parsing

--- 2024/d3/haskell.hs
+++ 2024/d3/haskell.hs
@@ -1,2 +1,56 @@
-import Data.Text.Regex
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE DeriveFunctor #-}
 
+import Control.Monad.Cont (when)
+import Data.List
+import Data.Maybe
+import Text.Read (readMaybe)
+
+newtype Parser a = Parser
+  { runParser :: String -> (String, Maybe a)
+  } deriving (Functor)
+  
+instance Applicative Parser where
+  pure c = Parser $ \s -> (s, Just c)
+  pf <*> pa = Parser $ \s -> case runParser pf s of
+    (s', Just f) -> fmap f <$> runParser pa s'
+    (s', Nothing) -> (s', Nothing)
+
+instance Monad Parser where
+    (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+    pa >>= f = Parser $ \s -> case runParser pa s of
+        (s', Just a) -> runParser (f a) s'
+        (s', Nothing) -> (s', Nothing)
+
+digit :: Parser Int
+digit = Parser $ \case
+    [] -> ("", Nothing)
+    (x:xs) -> (xs, readMaybe [x])
+
+char :: Char -> Parser ()
+char c = Parser $ \case
+    (x:xs) -> (xs, if x == c then Just () else Nothing)
+    [] -> ("", Nothing)
+
+str :: String -> Parser ()
+str = foldr1 (>>) . map char
+
+anyP :: Parser Char
+anyP = Parser $ \case
+  (x : xs) -> (xs, Just x)
+  [] -> ("", Nothing)
+
+eofP :: Parser ()
+eofP = Parser $ \case
+  [] -> ("", Just ())
+  x -> (x, Nothing)
+
+main = do
+  cat <- getContents
+  mapM_ print $ tokenizer tokens cat
+
+tokens = ["mul(", "don't", "do"] ++ [show x | x <- [0 .. 9]] ++ [[x] | x <- "(),"]
+
+tokenizer tokens = filter (\x -> any (`isPrefixOf` x) validStarts) . tails
+
+validStarts = ["mul(", "don't()", "do()"]
--- 2024/d3/odin.odin
+++ 2024/d3/odin.odin
@@ -4,16 +4,48 @@ import "../../lib"
 import "core:fmt"
 import "core:log"
 import "core:os"
+import "core:strconv"
 import "core:testing"
 import "core:text/regex"
 
 main :: proc() {
-	cat, _ := os.read_entire_file_from_filename("ex.txt")
-	rex, err := regex.create(`mul\(\d+,\d+\)`)
+	cat := lib.get_stdin()
+	rex, err := regex.create(`(?:mul\((\d+),(\d+)\))|(?:(?:do\(\))|(?:don't\(\)))`, {.Global})
 	defer regex.destroy(rex)
 	if err != nil {
-		log.fatal(err)
+		fmt.println(err)
 	}
-	matches, _ := regex.match(rex, string(cat))
-	fmt.println(matches, string(cat))
+    
+    dont := false
+	sum := 0
+	sum2 := 0
+	for cap in regex.match(rex, string(cat)) {
+		defer regex.destroy(cap)
+
+		pos := cap.pos[0]
+		cat = cat[pos[1]:]
+
+        if cap.groups[0] == "do()" {
+            dont = false
+        } else if cap.groups[0] == "don't()" {
+            dont = true
+        }
+
+        fmt.println(cap)
+
+		if len(cap.groups) != 3 {
+			continue
+		}
+
+		a, _ := strconv.parse_int(cap.groups[1])
+		b, _ := strconv.parse_int(cap.groups[2])
+        p := a*b
+		sum += p
+        if !dont {
+            sum2 += p
+        }
+	}
+
+	fmt.println(sum)
+	fmt.println(sum2)
 }
