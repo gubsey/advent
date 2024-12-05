ce3a0f05a41f6fd8c615849141535eb0bc745d0c
gubsey <tylertoohey@proton.me>
2024-12-04 19:23:37 -0500

    d4 p2: rewarding

--- 2024/d3/haskell.hs
+++ 2024/d3/haskell.hs
@@ -1,17 +1,19 @@
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFunctor, LambdaCase, TupleSections #-}
 
-import Control.Monad.Cont (when)
+import Control.Monad
+import Control.Applicative (liftA2)
 import Data.List
 import Data.Maybe
 import Text.Read (readMaybe)
+import Data.Char
+import Data.Functor
 
 newtype Parser a = Parser
   { runParser :: String -> (String, Maybe a)
   } deriving (Functor)
   
 instance Applicative Parser where
-  pure c = Parser $ \s -> (s, Just c)
+  pure c = Parser (, Just c)
   pf <*> pa = Parser $ \s -> case runParser pf s of
     (s', Just f) -> fmap f <$> runParser pa s'
     (s', Nothing) -> (s', Nothing)
@@ -22,35 +24,77 @@ instance Monad Parser where
         (s', Just a) -> runParser (f a) s'
         (s', Nothing) -> (s', Nothing)
 
-digit :: Parser Int
-digit = Parser $ \case
-    [] -> ("", Nothing)
-    (x:xs) -> (xs, readMaybe [x])
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy predicate = try $ do
+  c <- anyP
+  if predicate c
+    then pure c
+    else nullParser
 
-char :: Char -> Parser ()
-char c = Parser $ \case
-    (x:xs) -> (xs, if x == c then Just () else Nothing)
-    [] -> ("", Nothing)
+nullParser :: Parser a
+nullParser = Parser (, Nothing)
 
-str :: String -> Parser ()
-str = foldr1 (>>) . map char
+(<|>) :: Parser a -> Parser a -> Parser a
+p1 <|> p2 = Parser $ \s -> case runParser p1 s of
+  (s', Nothing)
+    | s' == s   -> runParser p2 s
+    | otherwise -> (s', Nothing)
+  success -> success
+
+many :: Parser a -> Parser [a]
+many  p = many' p <|> pure [] where
+  many' p = liftA2 (:) p $ many p
+
+try p = Parser $ \s -> case runParser p s of
+  (_s', Nothing) -> (s, Nothing)
+  success -> success
+
+char c = satisfy (==c)
+space = satisfy isSpace
+
+string :: String -> Parser String
+string = traverse char
 
 anyP :: Parser Char
 anyP = Parser $ \case
   (x : xs) -> (xs, Just x)
   [] -> ("", Nothing)
 
-eofP :: Parser ()
-eofP = Parser $ \case
-  [] -> ("", Just ())
-  x -> (x, Nothing)
+choice :: [Parser a] -> Parser a
+choice = foldr (<|>) nullParser
+
+number :: Parser Int
+number = read <$> many (satisfy isDigit)
+spaces = many $ satisfy isSpace
+symbol s = string s <* spaces
+
+
+run p s = snd $ runParser p s
+
+parseDo = string "do()"
+parseDont = string "don't()"
+parseMul = do
+  _ <- string "mul("
+  a <- number
+  _ <- char ','
+  b <- number
+  _ <- char ')'
+  pure $ a * b
+parse = choice 
+  [ Do <$ parseDo
+  , Dont <$ parseDont
+  , Mul <$> parseMul
+  ]
+
+
+
+data Parsed = Mul Int | Do | Dont
+instance Show Parsed where
+  show Do = "Do"
+  show Dont = "Don't"
+  show (Mul a) = show a
 
 main = do
   cat <- getContents
-  mapM_ print $ tokenizer tokens cat
-
-tokens = ["mul(", "don't", "do"] ++ [show x | x <- [0 .. 9]] ++ [[x] | x <- "(),"]
-
-tokenizer tokens = filter (\x -> any (`isPrefixOf` x) validStarts) . tails
-
-validStarts = ["mul(", "don't()", "do()"]
+  print . sum .mapMaybe (run parseMul) . tails $ cat
+  print . mapMaybe (run parse) . tails $ cat
--- 2024/d4/haskell.hs
+++ 2024/d4/haskell.hs
@@ -1,6 +1,7 @@
 import Control.Applicative (liftA2)
 import Data.Functor
 import Data.List
+import Numeric (showHex)
 
 printLists :: (Show a) => [[a]] -> IO ()
 printLists = mapM_ print
@@ -19,12 +20,21 @@ chunk n l = take n l : (chunk n . drop n $ l)
 
 diagonals =
   (++)
-    <$> transpose . zipWith drop [0 ..]
+    <$> reverse . transpose . zipWith drop [0 ..]
     <*> transpose . map reverse . zipWith take [0 ..]
 
+droppingDiags = map ((\[a, b] -> [dr a, dl b]) . replicate 2) . tails
+  where
+    dr = transpose . zipWith drop [0 ..]
+    dl = reverse . transpose . zipWith drop [0 ..] . map reverse
+
 a <||> b = liftA2 (||) a b
+
+makeLL n = take n . chunk n $ [10 ..]
+
+splitHalf l = splitAt ((length l + 1) `div` 2) l
 
 main = do
   ex <- lines <$> getContents
   print . length . filter (== "XMAS") . toNs 4 . concat . concatMap (\x -> [x, diagonals x]) . take 4 . iterate rotate $ ex
-  printLists . filter (all ((== "MAS") <||> (== "SAM"))) . transpose . map (toNs 3 . diagonals) . take 2 . iterate rotate $ ex
+  print . length . concatMap (filter (all ((== "MAS") <||> (== "SAM"))) . transpose . map (toNs 3 . map (take 3))) . droppingDiags $ ex
