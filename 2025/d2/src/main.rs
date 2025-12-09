5a6039443917bef257b1b092fdc0e7ba6a5ba275
gubsey <118205572+gubsey@users.noreply.github.com>
2025-12-09 14:30:34 +0000

    2025.d2.p2

--- 2025/d2/src/main.rs
+++ 2025/d2/src/main.rs
@@ -1,7 +1,7 @@
-use std::io::stdin;
+use std::{collections::HashSet, io::stdin};
 
 fn main() {
-    let p1: u64 = stdin()
+    let ids = stdin()
         .lines()
         .next()
         .unwrap()
@@ -9,8 +9,12 @@ fn main() {
         .split(',')
         .map(|s| s.split_once('-').unwrap())
         .flat_map(|(a, b)| a.parse::<u64>().unwrap()..=b.parse().unwrap())
-        .filter(|x| {
-            let digits = f64::log10(*x as f64).ceil() as usize;
+        .collect::<Vec<_>>();
+
+    let p1: u64 = ids
+        .iter()
+        .filter(|&&x| {
+            let digits = f64::log10(x as f64).ceil() as usize;
             if digits & 1 == 1 {
                 return false;
             }
@@ -22,5 +26,21 @@ fn main() {
         })
         .inspect(|x| println!("{x}"))
         .sum();
-    println!("p1: {p1}");
+
+    let p2: u64 = ids
+        .iter()
+        .filter(|&&x| {
+            let digits = f64::log10(x as f64).ceil() as usize;
+            let bytes = x.to_string().into_bytes();
+            for n in (1..=digits / 2).rev() {
+                let chunks = bytes.chunks(n).collect::<HashSet<_>>();
+                if chunks.len() == 1 {
+                    return true;
+                }
+            }
+            false
+        })
+        .inspect(|x| println!("{x}"))
+        .sum();
+    println!("p1: {p1}\np2: {p2}");
 }
