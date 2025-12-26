2e08599638e4bb8d8fad67933781224bcc4bd421
b3nt <brent@biggerluke.com>
2025-12-25 19:40:42 -0500

    2025-7-p2

--- src/bin/2025-7.rs
+++ src/bin/2025-7.rs
@@ -1,4 +1,7 @@
-use std::{collections::HashSet, io::stdin};
+use std::{
+    collections::{HashMap, HashSet},
+    io::stdin,
+};
 
 fn main() {
     let mut map = stdin()
@@ -15,8 +18,13 @@ fn main() {
         })
         .filter(|v| !v.is_empty());
     let start = map.next().unwrap();
-    let beams = map
-        .scan(start, |acc, mut v| {
+    let start_v = start.iter().copied().next().unwrap();
+    let sets = map.collect::<Vec<_>>();
+
+    let p1_splits = sets
+        .iter()
+        .scan(start, |acc, v| {
+            let mut v = v.clone();
             v.retain(|x| acc.contains(x));
             let splits = v.len();
             acc.retain(|x| !v.contains(x));
@@ -24,5 +32,19 @@ fn main() {
             Some(splits)
         })
         .collect::<Vec<_>>();
-    println!("{}", beams.iter().copied().sum::<usize>());
+    let mut timelines = HashMap::new();
+    timelines.insert(start_v, 1i128);
+    for set in sets {
+        for item in set {
+            if let Some(x) = timelines.remove(&item) {
+                *timelines.entry(item - 1).or_default() += x;
+                *timelines.entry(item + 1).or_default() += x;
+            }
+        }
+    }
+    println!(
+        "p1: {}\np2: {}",
+        p1_splits.iter().copied().sum::<usize>(),
+        timelines.into_values().sum::<i128>()
+    );
 }
