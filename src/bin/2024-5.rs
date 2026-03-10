b605edd10b884a94dfd02f62cb4b7f747fb52d8d
gubsey <118205572+gubsey@users.noreply.github.com>
2026-03-10 15:19:16 +0000

    2025-5

--- src/bin/2024-5.rs
+++ src/bin/2024-5.rs
@@ -16,40 +16,36 @@ fn main() {
         .lines()
         .map(|l| l.split(',').map(|s| s.parse().unwrap()).collect())
         .collect();
-
-    println!(
-        "p1: {}\np2: {}",
-        updates
-            .iter()
-            .filter_map(|v| {
-                for i in 0..v.len() {
-                    let k = v[i];
-                    let s = &v[i + 1..];
-                    for x in s {
-                        if rules.contains(&(*x, k)) {
-                            return None;
-                        }
+    let p1 = updates
+        .iter()
+        .filter_map(|v| {
+            for i in 0..v.len() {
+                let k = v[i];
+                let s = &v[i + 1..];
+                for x in s {
+                    if rules.contains(&(*x, k)) {
+                        return None;
                     }
                 }
-                Some(v[v.len() / 2])
-            })
-            .sum::<usize>(),
-        updates
-            .iter_mut()
-            .filter_map(|u| {
-                let mut wrong = false;
-                for i in 0..u.len() {
-                    for j in i + 1..u.len() {
-                        let a = u[i];
-                        let b = u[j];
-                        if rules.contains(&(b, a)) {
-                            u.swap(i, j);
-                            wrong = true;
-                        }
-                    }
+            }
+            Some(v[v.len() / 2])
+        })
+        .sum::<usize>();
+    let p2 = updates
+        .iter_mut()
+        .filter_map(|u| {
+            let mut wrong = false;
+            let len = u.len();
+            for [i, j] in (0..len - 1).flat_map(move |i| (i + 1..len).map(move |j| [i, j])) {
+                let a = u[i];
+                let b = u[j];
+                if rules.contains(&(b, a)) {
+                    u.swap(i, j);
+                    wrong = true;
                 }
-                wrong.then(|| u[u.len() / 2])
-            })
-            .sum::<usize>()
-    );
+            }
+            wrong.then(|| u[u.len() / 2])
+        })
+        .sum::<usize>();
+    println!("p1: {p1}\np2: {p2}");
 }
